5483c85972c97e631d0deec1f1273ff83f2b512c
adabs-urdum <cyrill.lehmann@clus.ch>
2019-04-03 22:07:54 +0200

    cleanup, obj update, window resize

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,88 @@
 function WebGLThreeJS(){
+
+  let scene,
+      legoObjPromise,
+      camera,
+      hemisphereLight,
+      directionalLight,
+      pointLight,
+      spotLight,
+      head,
+      torso,
+      handRight,
+      armRight,
+      handLeft,
+      armLeft,
+      legLeft,
+      legRight,
+      pants,
+      renderer;
+
   const THREE = require('three');
   const OBJLoader = require('three-obj-loader');
-  const MTLLoader = require('three-mtl-loader');
   OBJLoader(THREE);
+  const MTLLoader = require('three-mtl-loader');
 
-  const scene = new THREE.Scene();
+  function init(){
+    setVars();
+    bindEvents();
+    addLegoObj();
+    mainLoop();
+  };
 
-  var myObjPromise = loadObj( "/dist/obj/", "lego" );
+  function setVars(){
 
-  const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 501);
-  camera.position.set(0,13,20);
-  camera.lookAt( 0,0,0 );
+    scene = new THREE.Scene();
 
-  const skyColor = 0xa0bcd7;
-  const groundColor = 0xbfbfbf;
-  const intensity = 1;
-  const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-  scene.add(light);
+    legoObjPromise = loadObj( "/dist/obj/", "lego" );
 
-  const color = 0xDBDBDB;
-  const intensity2 = 0.8;
-  const light2 = new THREE.DirectionalLight(color, intensity);
-  light2.position.set(0, 50, 0);
-  light2.target.position.set(0, 0, 0);
-  scene.add(light2);
-  scene.add(light2.target);
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 501);
+    camera.position.set(0,10,20);
+    camera.lookAt( 0,0,0 );
 
-  const pointLight = new THREE.PointLight(0xcccccc, 0.1, 0, 2);
-  pointLight.position.set(-6,-6,4)
-  scene.add(pointLight);
+    const skyColor = 0xa0bcd7;
+    const groundColor = 0xbfbfbf;
+    hemisphereLight = new THREE.HemisphereLight(skyColor, groundColor, 1);
+    scene.add(hemisphereLight);
 
-  const spotLight = new THREE.SpotLight(0xededed, 0.2, 0, 1, 1, 1);
-  spotLight.position.set(12,8,8);
-  scene.add(spotLight);
+    directionalLight = new THREE.DirectionalLight(0xDBDBDB, 1);
+    directionalLight.position.set(0, 50, 0);
+    directionalLight.target.position.set(0, 0, 0);
+    scene.add(directionalLight);
 
-  myObjPromise.then(myObj => {
-    spotLight.target = myObj;
-  });
+    pointLight = new THREE.PointLight(0xcccccc, 0.1, 0, 2);
+    pointLight.position.set(-5,-5,6)
+    scene.add(pointLight);
 
-  function loadObj( path, name ){
+    spotLight = new THREE.SpotLight(0xa9d1ff, 0.2, 0, 1, 1, 1);
+    spotLight.position.set(12,8,8);
+    scene.add(spotLight);
+    legoObjPromise.then(legoObj => {
+      spotLight.target = legoObj;
+    });
 
-    var progress = console.log;
+    renderer = new THREE.WebGLRenderer({
+      alpha: true,
+      antialias: true,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+  }
+
+  function bindEvents(){
+    window.addEventListener('resize', resizeRenderer);
+  }
+
+  function resizeRenderer(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  function loadObj(path, name){
+
+    let progress = console.log;
 
     return new Promise(function( resolve, reject ){
 
@@ -63,74 +105,59 @@ function WebGLThreeJS(){
 
   }
 
+  function addLegoObj(){
+    legoObjPromise.then(legoObj => {
 
-  myObjPromise.then(myObj => {
+      scene.add( legoObj );
 
-    scene.add( myObj );
+      legoObj.traverse(function(child){
+        console.log(child);
+        if (child instanceof THREE.Mesh){
+          child.material.side = THREE.DoubleSide;
+        }
+      });
 
-    // console.log('children START');
-    myObj.traverse(function(child){
-      console.log(child);
-      if (child instanceof THREE.Mesh){
-        console.log(child.material.uuid);
-        child.material.side = THREE.DoubleSide;
-    //     console.log('is THREE.Mesh');
-    //     console.log(child.name);
-    //     if(child.name == 'HandLeft_Cylinder.001'){
-    //       console.log('if');
-    //       child.material.color.setHex(0xffdb4d);
-    //     }
-    //     else{
-    //       console.log('else');
-    //       child.material.color.setHex(0x818181);
-    //     }
-      }
-    //   console.log('-------------------');
+      head = legoObj.getObjectByName('Head');
+      head.material.color.setHex(0xffdb4d);
+
+      torso = legoObj.getObjectByName('Torso');
+      torso.material.color.setHex(0x006666);
+
+      handRight = legoObj.getObjectByName('HandRight');
+      handRight.material.color.setHex(0xffdb4d);
+
+      armRight = legoObj.getObjectByName('ArmRight');
+      armRight.material.color.setHex(0x006666);
+
+      handLeft = legoObj.getObjectByName('HandLeft');
+      handLeft.material.color.setHex(0xffdb4d);
+
+      armLeft = legoObj.getObjectByName('ArmLeft');
+      armLeft.material.color.setHex(0x006666);
+
+      legLeft = legoObj.getObjectByName('LegLeft');
+      legLeft.material.color.setHex(0x800040);
+
+      legRight = legoObj.getObjectByName('LegRight');
+      legRight.material.color.setHex(0x800040);
+
+      pants = legoObj.getObjectByName('Pants');
+      pants.material.color.setHex(0x000000);
+
     });
-    // console.log('children END');
-
-    const head = myObj.getObjectByName('Head');
-    head.material.color.setHex(0xffdb4d);
-
-    const torso = myObj.getObjectByName('Torso');
-    torso.material.color.setHex(0x006666);
-
-    const handRight = myObj.getObjectByName('HandRight');
-    handRight.material.color.setHex(0xffdb4d);
-
-    const armRight = myObj.getObjectByName('ArmRight');
-    armRight.material.color.setHex(0x006666);
-
-    const handLeft = myObj.getObjectByName('HandLeft');
-    handLeft.material.color.setHex(0xffdb4d);
-
-    const armLeft = myObj.getObjectByName('ArmLeft');
-    armLeft.material.color.setHex(0x006666);
-
-    const legLeft = myObj.getObjectByName('LegLeft');
-    legLeft.material.color.setHex(0x800040);
-
-    const legRight = myObj.getObjectByName('LegRight');
-    legRight.material.color.setHex(0x800040);
-
-  });
-
-  const renderer = new THREE.WebGLRenderer({
-    alpha: true
-  });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  }
 
   function mainLoop(){
-    myObjPromise.then(myObj => {
-      myObj.rotation.y += 0.01;
+    legoObjPromise.then(legoObj => {
+      legoObj.rotation.y += 0.005;
     });
 
     renderer.render(scene, camera);
     requestAnimationFrame(mainLoop);
   }
 
-  mainLoop();
+  init();
+
 }
 
 WebGLThreeJS();
